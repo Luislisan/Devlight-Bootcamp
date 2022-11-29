250f0a746b55fa60f6b9b76f5d5480a95dca3c7b
Luislisan <lisandro99@hotmail.es>
2022-11-29 15:50:38 -0300

    first commit

---
+++ tarea.js
@@ -0,0 +1,176 @@
+// Devlight
+/* 12345 */
+
+var myName;
+// Setup
+var a;
+
+a = 7;
+
+// Only change code below this line
+// Setup
+var a;
+a = 7;
+var b;
+b = a;
+
+// Only change code below this line
+var a = 9;
+var myFirstName = "Lisandro"
+var myLastName = "Sanchez"
+// Only change code below this line
+
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
+const sum = 10 + 10;
+
+const difference = 45 - 33;
+
+const product = 8 * 10;
+
+const quotient = 66 / 33;
+
+let myVar = 87;
+
+// Only change code below this line
+myVar = ++myVar;
+let myVar = 11;
+
+// Only change code below this line
+myVar = --myVar;
+const ourDecimal = 5.7;
+
+// Only change code below this line
+const myDecimal = 0.3;
+
+const product = 2.0 * 2.5;
+
+const quotient = 4.4 / 2.0; // Change this line
+
+const remainder = 11 % 3;
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a = a += 12;
+b = b += 9;
+c = c += 7;
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a = a -= 6;
+b = b -= 15;
+c = c -= 1;
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a = a *= 5;
+b = b *= 3;
+c = c *= 10;
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a = a /= 12;
+b = b /= 4;
+c = c /= 11;
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+console.log(myStr)
+
+const myStr = "This is the start." + " This is the end."; // Change this line
+let myStr = "This is the first sentence.";
+
+myStr += " This is the second sentence.";
+
+// Only change code below this line
+const myName = " Lisandro";
+let myStr = "My name is" + myName + "and I am well!";
+
+// Change code below this line
+const someAdjective = "Lovely";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective;
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "My " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Change this line
+// Only change code above this line
+
+// Only change code below this line
+const myArray = ["Pancho", 2];
+
+// Only change code below this line
+const myArray = [["Pancho", "Papas"], ["hamburguesa", "Cheddar"]];
